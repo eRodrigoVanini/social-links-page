fbfd609116e7f6e8a92836aec6551687e9f90b5b
eRodrigoVanini <arch.rvanini@gmail.com>
2025-10-09 21:36:35 -0300

    Alterações.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const botaoAtivo = function ativaBotao () {
-    document.addEventListener(click)
-    return ativaBotao
-};
+
+
+const botao01 = document.getElementById("switchbutton");
+
+const html01 = document.querySelector('html');
+
+botao01.addEventListener("click", () => {html01.classList.toggle('light')});
