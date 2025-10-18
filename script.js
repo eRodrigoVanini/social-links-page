11934be15a12753a542081b9227d82f7454beecf
eRodrigoVanini <arch.rvanini@gmail.com>
2025-10-17 23:36:39 -0300

    Finaliza versao01 do aplicativo

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-
-
+const html01 = document.querySelector('html');
 const botao01 = document.getElementById("switchbutton");
 
-const html01 = document.querySelector('html');
 
 botao01.addEventListener("click", () => {html01.classList.toggle('light')});
+
+
+
+//capture tag image
+const img = document.querySelector("#profile img")
+
+
+
+
+
+
+//replace image
+if(html01.classList.contains('light')) {
+//if light mode
+img.setAttribute('src', './assets/avatar-light.svg')
+} else {
+img.setAttribute('src', './assets/avatar.svg')
+}
+//if dark mode
+
+
